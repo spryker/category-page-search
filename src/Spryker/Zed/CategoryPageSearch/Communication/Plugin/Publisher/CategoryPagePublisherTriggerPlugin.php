5a4d1846a387656cbd6e1182f03582d088d1dc5a
Roman Havrylko <roman.havrylko@spryker.com>
2021-03-26 15:34:26 +0200

    CC-12664 Replaced CategoryPageSearch plugins.

---
+++ src/Spryker/Zed/CategoryPageSearch/Communication/Plugin/Publisher/CategoryPagePublisherTriggerPlugin.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\CategoryPageSearch\Communication\Plugin\Publisher;
+
+use Generated\Shared\Transfer\FilterTransfer;
+use Orm\Zed\Category\Persistence\Map\SpyCategoryNodeTableMap;
+use Spryker\Shared\CategoryPageSearch\CategoryPageSearchConstants;
+use Spryker\Zed\Kernel\Communication\AbstractPlugin;
+use Spryker\Zed\PublisherExtension\Dependency\Plugin\PublisherTriggerPluginInterface;
+
+/**
+ * @method \Spryker\Zed\CategoryPageSearch\Business\CategoryPageSearchFacadeInterface getFacade()
+ * @method \Spryker\Zed\CategoryPageSearch\Persistence\CategoryPageSearchQueryContainerInterface getQueryContainer()
+ * @method \Spryker\Zed\CategoryPageSearch\Communication\CategoryPageSearchCommunicationFactory getFactory()
+ * @method \Spryker\Zed\CategoryPageSearch\CategoryPageSearchConfig getConfig()
+ */
+class CategoryPagePublisherTriggerPlugin extends AbstractPlugin implements PublisherTriggerPluginInterface
+{
+    /**
+     * {@inheritDoc}
+     * - Retrieves category nodes by provided limit and offset.
+     *
+     * @api
+     *
+     * @param int $offset
+     * @param int $limit
+     *
+     * @return \Generated\Shared\Transfer\NodeTransfer[]
+     */
+    public function getData(int $offset, int $limit): array
+    {
+        $filterTransfer = $this->createFilterTransfer($offset, $limit);
+
+        return $this->getFactory()
+            ->getCategoryFacade()
+            ->findCategoryNodesByFilter($filterTransfer)
+            ->getNodes()
+            ->getArrayCopy();
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @api
+     *
+     * @return string
+     */
+    public function getResourceName(): string
+    {
+        return CategoryPageSearchConstants::CATEGORY_NODE_RESOURCE_NAME;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @api
+     *
+     * @return string
+     */
+    public function getEventName(): string
+    {
+        return CategoryPageSearchConstants::CATEGORY_NODE_PUBLISH;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @api
+     *
+     * @return string|null
+     */
+    public function getIdColumnName(): ?string
+    {
+        return SpyCategoryNodeTableMap::COL_ID_CATEGORY_NODE;
+    }
+
+    /**
+     * @param int $offset
+     * @param int $limit
+     *
+     * @return \Generated\Shared\Transfer\FilterTransfer
+     */
+    protected function createFilterTransfer(int $offset, int $limit): FilterTransfer
+    {
+        return (new FilterTransfer())
+            ->setOffset($offset)
+            ->setLimit($limit);
+    }
+}
